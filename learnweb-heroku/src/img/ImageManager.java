2b1b09050c5037a94903503a11efb7e388fc5e56
tatsumakimorosuke <zigzagmorokichi@gmail.com>
2021-08-08 23:57:36 +0900

    second commit

--- learnweb-heroku/src/img/ImageManager.java
+++ learnweb-heroku/src/img/ImageManager.java
@@ -1,5 +1,7 @@
 package img;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,26 +11,44 @@ import java.sql.SQLException;
 import shopping.Item;
 import shopping.ItemImageView;
 
-public class ImageManager implements ItemImageView{
-	String JDBC_URL = "jdbc:h2:tcp://localhost/~/learnweb";
-	String DB_USER = "sa";
-	String DB_PASS = "";
+public class ImageManager implements ItemImageView {
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		// heroku configに設定されている値を取得。
+		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+		// :をデリミタとして必要な情報を抜き取る。
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		// JDBC用のURLを生成。
+		String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+		return DriverManager.getConnection(dbUrl, username, password);
+	}
+
 	@Override
 	public void itemImage(Item item) {
-		// TODO 自動生成されたメソッド・スタブ
-		String sql = "SELECT PRODUCT_IMG FROM PRODUCT_IMG WHERE PRODUCT_CD = ?";
-		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
-				PreparedStatement pSmt = conn.prepareStatement(sql)) {
-			pSmt.setString(1, item.getProductCd());
+		try {
+			// ドライバの読み込み。
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// データベースへ接続のためprivateメソッドを呼び出す。
+			String sql = "SELECT PRODUCT_IMG FROM PRODUCT_IMG WHERE PRODUCT_CD = ?";
+			try (Connection conn = getConnection();
+					PreparedStatement pSmt = conn.prepareStatement(sql)) {
+				pSmt.setString(1, item.getProductCd());
 
-			ResultSet rs = pSmt.executeQuery();
+				ResultSet rs = pSmt.executeQuery();
 
-			while (rs.next()) {
+				while (rs.next()) {
 
-				item.setProductImg(rs.getString("PRODUCT_IMG"));
+					item.setProductImg(rs.getString("PRODUCT_IMG"));
+				}
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+
 		}
 	}
 
